9d83c08aec25e4949c667c5859e40ab12f7556af
savvax <savvax@gmail.com>
2020-01-25 17:36:51 +0300

    25.01.2020 2

--- src/main/java/com/ifmojava/springweb/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ifmojava.springweb.repository;
-
-import com.ifmojava.springweb.entity.Event;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventRepository
-        extends CrudRepository<Event, Integer> {
-}
