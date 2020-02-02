27f0886134344591a47cf96e81db85f629889f9a
savvax <savvax@gmail.com>
2020-02-02 19:35:23 +0300

    cmc addition html optimisation

---
+++ src/main/java/com/ifmojava/dplmprjct/cmc/CMCListingLastest.java
@@ -0,0 +1,67 @@
+package com.ifmojava.dplmprjct.cmc;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+class CMCListingLatest {
+
+    private static String apiKey = "a47fec7a-2a44-4f60-a72f-f24e10fd89fe";
+
+    public static void main(String[] args) {
+        String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+        List<NameValuePair> paratmers = new ArrayList<NameValuePair>();
+        paratmers.add(new BasicNameValuePair("start","1"));
+        paratmers.add(new BasicNameValuePair("limit","1"));
+        paratmers.add(new BasicNameValuePair("convert","USD"));
+
+        try {
+            String result = makeAPICall(uri, paratmers);
+            System.out.println(result);
+        } catch (IOException e) {
+            System.out.println("Error: cannont access content - " + e.toString());
+        } catch (URISyntaxException e) {
+            System.out.println("Error: Invalid URL " + e.toString());
+        }
+    }
+
+    public static String makeAPICall(String uri, List<NameValuePair> parameters)
+            throws URISyntaxException, IOException {
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return response_content;
+    }
+
+}
--- src/main/java/com/ifmojava/dplmprjct/controllers/SomeController.java
+++ src/main/java/com/ifmojava/dplmprjct/controllers/SomeController.java
@@ -1,13 +1,41 @@
 package com.ifmojava.dplmprjct.controllers;
 
+import com.ifmojava.dplmprjct.entity.University;
+import com.ifmojava.dplmprjct.repository.UniversityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class SomeController {
 
-    @RequestMapping("/")
-    public String index(){
+//    @RequestMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
+
+    private UniversityRepository universityRepository;
+
+    @Autowired
+    public SomeController(UniversityRepository universityRepository) {
+        this.universityRepository = universityRepository;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showForm(Model model){
+        model.addAttribute("university", new University());
         return "index";
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute University university){
+        universityRepository.save(university);
+        return "index";
+    }
+
 }
